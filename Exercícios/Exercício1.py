6aca4e438bf3d9ac69250c1d66c2f86e1536e3be
Samuel Watthier <98723071+9Gaucho@users.noreply.github.com>
2024-08-14 19:07:11 -0300

    Update Exercício1.py

--- Exercícios/Exercício1.py
+++ Exercícios/Exercício1.py
@@ -1,1 +1,9 @@
-
+public class Ex1 {
+    public static void main(String[] args){
+        float saldoinicial = 15;
+        float percentjuros = 10;
+        float meses = 6;
+        float aplicacao = saldoinicial *(1 + percentjuros/100) math.pow(meses);
+        
+        System.out.println(aplicacao);
+    }
